f3ea64a530d180dc2948a9bfbf548063b1570ba5
JayaramTechnicalStack <62337379+JayaramTechnicalStack@users.noreply.github.com>
2024-07-22 17:36:16 +0530

    Update CoffeeDayMahine.java
    hot drinks added

--- CoffeeDayMahine.java
+++ CoffeeDayMahine.java
@@ -32,6 +32,8 @@ import java.util.Scanner;
 	    break; 
 	    case 6: System.out.println("Please have cooldirinks");  
 	    break;
+	    case 7: System.out.println("Please have hot drinks");  
+	    break;
 	    default:System.out.println("Invalid Option you have Enter, please choose valid option!");    
 	    }     
 
