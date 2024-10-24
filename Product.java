5bba5b22e6a57fd18e5f5965aa950dcae9e9e9af
Jayaram Reddy <reddy.jay28@gmail.com>
2024-10-24 20:43:48 +0530

    getProductDetailsById method Added

--- Product.java
+++ Product.java
@@ -4,6 +4,10 @@ private int prodId;
 private String prodName;
 private double price;
 
+public void getProductDetailsById(int product){
+System.out.println("Getting product detail info");
+}
+
 public static void main(String [] args){
 }
 } 
