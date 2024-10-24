9494ecc05741e8649d05e50b5f8b2f0633095d93
JayaramTechnicalStack <62337379+JayaramTechnicalStack@users.noreply.github.com>
2024-10-24 20:41:32 +0530

    Update Product.java

--- Product.java
+++ Product.java
@@ -4,6 +4,10 @@ private int prodId;
 private String prodName;
 private double price;
 
+  public void createProduct(){
+  System.out.pritnln("Product is create");
+  }
+
 public static void main(String [] args){
 }
 } 
