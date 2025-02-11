dd06029fa1971008144099fb32a92e4c09c0f0f4
JayaramTechnicalStack <62337379+JayaramTechnicalStack@users.noreply.github.com>
2025-02-11 20:06:46 +0530

    Update Client.java
    added code changes

--- Client.java
+++ Client.java
@@ -9,9 +9,18 @@ public class Client {
 		e.setEname("Jack");
 		e.setSalary(10000.0);
 		e.setEaddress("xyz@gmail.com");
+
+
+		Employee e2=new Employee();
 		
+		e2.setEmpid(101);
+		e2.setEname("Jack");
+		e2.setSalary(10000.0);
+		e2.setEaddress("xyz@gmail.com");
 		System.out.println("Employee id is :"+e.getEmpid() + " Employee Name is :"+e.getEname() +
 		"Employee Salary is :"+e.getSalary()+ " Employee Email-id is " +e.getEaddress());
+		System.out.println("Employee id is :"+e2.getEmpid() + " Employee Name is :"+e2.getEname() +
+		"Employee Salary is :"+e2.getSalary()+ " Employee Email-id is " +e2.getEaddress());
 		
 	}
 
