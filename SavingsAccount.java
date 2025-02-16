e59e9f02837880b2fb0d816afd88a3aaa6588372
Jayaram Reddy <reddy.jay28@gmail.com>
2025-02-16 17:31:46 +0530

    parameter constructor added

--- SavingsAccount.java
+++ SavingsAccount.java
@@ -3,7 +3,11 @@ public class Account {
 private String accountId;
 private String accountHolderName;
 private String ifscCode;
-public Account(){
+public Account(String accountId,String accountHolderName,String ifscCode){
+
+this.accountId= accountId;
+this.accountHolderName=accountHolderName;
+this.ifscode = ifscode;
 
 }
 }
