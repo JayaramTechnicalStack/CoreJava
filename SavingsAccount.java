5b1b15587cb59cd6b68a20147af4fd9410850b96
Jayaram Reddy <reddy.jay28@gmail.com>
2025-02-16 17:21:30 +0530

    accounts code added

---
+++ SavingsAccount.java
@@ -0,0 +1,15 @@
+public class Account {
+
+private String accountId;
+private String accountHolderName;
+private String ifscCode;
+public Account(){
+
+}
+}
+
+public class SavingsAccount extends Account{
+
+public static void main(String [] args){
+}
+}
