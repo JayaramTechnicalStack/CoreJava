3d82cc00cbdc8ff7b7a4f2776599d2f61536e021
Jayaram Reddy <reddy.jay28@gmail.com>
2025-06-26 20:24:27 +0530

    added address filed and created new class

--- SavingsAccount.java
+++ SavingsAccount.java
@@ -3,6 +3,7 @@ public class Account {
 private String accountId;
 private String accountHolderName;
 private String ifscCode;
+private String address;
 public Account(String accountId,String accountHolderName,String ifscCode){
 
 this.accountId= accountId;
@@ -10,6 +11,16 @@ this.accountHolderName=accountHolderName;
 this.ifscode = ifscode;
 
 }
+
+public Account(String accountId,String accountHolderName,String ifscCode,String address){
+
+this.accountId= accountId;
+this.accountHolderName=accountHolderName;
+this.ifscode = ifscode;
+this.address = address;
+
+}
+
 }
 
 public class SavingsAccount extends Account{
