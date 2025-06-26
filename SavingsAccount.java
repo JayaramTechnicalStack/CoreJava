1546cab085b89297f47df309477991c0701da9e6
Jayaram Reddy <reddy.jay28@gmail.com>
2025-06-26 20:42:07 +0530

    balance filed added in class

--- SavingsAccount.java
+++ SavingsAccount.java
@@ -4,6 +4,7 @@ private String accountId;
 private String accountHolderName;
 private String ifscCode;
 private String address;
+private double balance;
 public Account(String accountId,String accountHolderName,String ifscCode){
 
 this.accountId= accountId;
@@ -12,13 +13,13 @@ this.ifscode = ifscode;
 
 }
 
-public Account(String accountId,String accountHolderName,String ifscCode,String address){
+public Account(String accountId,String accountHolderName,String ifscCode,String address, double balance){
 
 this.accountId= accountId;
 this.accountHolderName=accountHolderName;
 this.ifscode = ifscode;
 this.address = address;
-
+this.balance = balance;
 }
 
 }
